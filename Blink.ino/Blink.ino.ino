411705f6e23402b295906b387564a2157ff98d74
Lucas Campbell <lucass.ccampbell@gmail.com>
2023-03-06 14:14:17 -0500

    Added pin 12

--- Blink.ino/Blink.ino.ino
+++ Blink.ino/Blink.ino.ino
@@ -30,16 +30,26 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(500);                       // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
+     digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(250);  
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(2000);  
+  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
+  delay(2000);  //
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
   delay(1500);  // wait for a second
 }
