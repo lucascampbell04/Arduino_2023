83d14c116f08e09676997b653585acc090def402
Lucas Campbell <lucass.ccampbell@gmail.com>
2023-03-16 14:07:48 -0400

    Initial BlindSpotMonitor Commit

---
+++ BlindSpotMonitor/BlindSpotMonitor.ino
@@ -0,0 +1,18 @@
+// Programmer: Lucas Campbell
+// Date: 3/16/2023
+// Program: BlindSpotSensor
+
+/*
+ * This program will determine distances approaching vehicle that are in your blind spot and turn on a warning light
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
