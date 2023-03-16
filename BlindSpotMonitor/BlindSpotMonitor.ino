75897dc3c356dd62830accc73d9ecfee5331c3f3
Lucas Campbell <lucass.ccampbell@gmail.com>
2023-03-16 14:13:14 -0400

    Add resource to comments

--- BlindSpotMonitor/BlindSpotMonitor.ino
+++ BlindSpotMonitor/BlindSpotMonitor.ino
@@ -1,6 +1,7 @@
 // Programmer: Lucas Campbell
 // Date: 3/16/2023
-// Program: BlindSpotSensor
+// Program: BlindSpotMonitor
+// Resource: https://projecthub.arduino.cc/Krepak/a6ea3ad5-fd23-49c9-a96c-5e6fcf97f2cc?ref=search&ref_id=red%20green%20yellow%20light%20echo%20sensor&offset=1
 
 /*
  * This program will determine distances approaching vehicle that are in your blind spot and turn on a warning light
